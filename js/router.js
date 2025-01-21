6a98eed7a62151950fd8e6fd9ffaa49f1ee6247e
Alfred Narjes <alfred.narjes@gmail.com>
2025-01-21 20:28:05 +0100

    added 404 translation, changed language recognition, deleted some comments

--- js/router.js
+++ js/router.js
@@ -10,8 +10,6 @@ const routes = {
 }
 
 const handleLocation = async () => {
-  // const path = window.location.pathname;
-  // const route = routes[path] || routes[404];
 
   // Falls der Nutzer noch keinen Hash hat (z. B. erste Seite: #/ ), setze standardmäßig #/
   if (!window.location.hash) {
@@ -39,72 +37,19 @@ const handleLocation = async () => {
     console.log(document.querySelector("#app"));
 
     document.querySelector("#app").innerHTML = await fetch(routes[404]).then((data) => data.text());
-    //     document.querySelector("#app").innerHTML = `
-    //     <section>
-    //         <h1>Fehler</h1>
-    //         <p>Die angeforderte Seite konnte nicht geladen werden.</p>
-    //     </section>
-    // `;
+
   }
 
-  // console.log(route);
-
-  // const html = await fetch(route).then(response => response.text());
-  // document.querySelector("#app").innerHTML = html;>
 }
 
 const route = (event) => {
-  // event = event || window.event;
+
   // event.preventDefault();
-  // window.history.pushState({}, "", event.target.href);
-  // handleLocation();
-  event.preventDefault();
-  // Wir setzen einfach den hash:
-  window.location.hash = event.target.getAttribute("href").replace("#", "");
-  // Dann Laden:
+  // window.location.hash = event.target.getAttribute("href").replace("#", "");
   handleLocation();
 }
 
-// const getBreadCrumbsPath = () => {
-//     const crumbs = window.location.pathname
-//       .split("/")
-//       .filter(Boolean); // Entfernt leere Einträge, z. B. vor oder nach dem Slash
-
-//     return crumbs
-//       .map((crumb, index) => {
-//         const path = "/" + crumbs.slice(0, index + 1).join("/");
-
-//         const separator = index > 0 ? " / " : "";
-//         console.log(`${separator}<a href="${path}" onclick="route()">${crumb}</a>`);
-
-//         return `${separator}<a href="${path}" onclick="route()">${crumb}</a>`;
-//       })
-//       .join("");
-//   };
 const getBreadCrumbsArray = () => {
-  // if (window.location.pathname === "/") {
-  //   return [{ name: "Home", path: "#/" }];
-  // }
-  // const segments = window.location.pathname
-  //   .split('/')
-  //   .filter(Boolean);
-
-  // // Erzeuge ein Array von Objekten { name, path }
-  // const breadCrumbs = segments.map((segment, index) => {
-  //   // Den absoluten Pfad bis einschließlich dieses Segments zusammensetzen
-  //   const path = '/' + segments.slice(0, index + 1).join('/').toLowerCase();
-  //   const name =
-  //     segment.charAt(0).toUpperCase() + segment.slice(1).toLowerCase();
-  //   return {
-  //     name: name,
-  //     path: path
-  //   };
-  // });
-
-  // // console.log(breadCrumbs);
-
-  // return breadCrumbs;
-
   // Aktuellen Hash auslesen, z. B. "#/projects/example"
   let hash = window.location.hash;
   if (!hash || hash === "#/") {
